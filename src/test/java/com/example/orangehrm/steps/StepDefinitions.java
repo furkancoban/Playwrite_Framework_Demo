24bec84de7aad892500d06be7ff8d46a8473df67
afurk <afurk@example.com>
2026-03-02 13:51:25 -0600

    Suppresses unused warnings in step definitions

--- src/test/java/com/example/orangehrm/steps/StepDefinitions.java
+++ src/test/java/com/example/orangehrm/steps/StepDefinitions.java
@@ -24,6 +24,7 @@ import io.cucumber.java.en.When;
  * The assertions are pretty straightforward - each step just checks that its main
  * action worked. More complex validation lives in the page objects themselves.
  */
+@SuppressWarnings("unused")
 public class StepDefinitions {
 
     private TestContext testContext;
